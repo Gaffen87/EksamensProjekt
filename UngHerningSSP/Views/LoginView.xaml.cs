6df250546ef41b8ca0fb4f9c954de51830f63b1e
Nikoline Brink Kjærsgaard <nbkj54129@edu.ucl.dk>
2023-12-05 09:45:08 +0100

    login

--- UngHerningSSP/Views/LoginView.xaml.cs
+++ UngHerningSSP/Views/LoginView.xaml.cs
@@ -24,4 +24,53 @@ public partial class LoginView : Page
 	{
 		InitializeComponent();
 	}
+
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+        string username = UsernameTextBox.Text;
+        string password = PasswordBox.Password;
+        string path = "User.txt";
+
+
+        if (username == "test" && password == "test")
+        {
+
+            // TODO: Add code to navigate to the main application window or perform other actions.
+            NavigationService.Navigate(new UserMapView());
+        }
+
+
+        //if (username.Length <= 1 || password.Length <= 1 || username == null || password == null)
+        //{
+        //    MessageBox.Show("Login failed. Please try again.");
+
+        //}
+
+        //else if (File.Exists(path))
+        //{
+        //    string[] lines = File.ReadAllLines(path);
+        //    bool matchFound = false;
+
+        //    foreach (string line in lines)
+        //    {
+        //        if (line.Contains(UsernameTextBox.Text) && line.Contains(PasswordBox.Password) && line.Contains("True"))
+        //        {
+        //            NavigationService.Navigate(new Files());
+        //            matchFound = true;
+        //            break;
+        //        }
+        //        else if (line.Contains(UsernameTextBox.Text) && line.Contains(PasswordBox.Password) && line.Contains("False"))
+        //        {
+        //            NavigationService.Navigate(new FilesNoAdmin());
+        //            matchFound = true;
+        //            break;
+        //        }
+        //    }
+
+        //    if (!matchFound)
+        //    {
+        //        MessageBox.Show("Login failed. Please try again.");
+        //    }
+        //}
+    }
 }
